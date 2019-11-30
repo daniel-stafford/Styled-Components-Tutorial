c663d7afc3cdc7b26f7b845f111ca18c3d5548d9
Daniel-Stafford <stafford.daniel@gmail.com>
2019-11-30 18:08:19 +0200

    lesson 6

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+const Heading = styled.h1`
+  font-size: 2rem;
+`
+
+const color = 'white'
+
+const Button = styled.button`
+  background: indigo;
+  padding: 5px 10px;
+  border-radius: 4px;
+  border: none;
+  color: ${color};
+  font-size: 2rem;
+  background: ${({ type }) => (type === 'cancel' ? 'tomato' : 'indigo')};
+`
+
+const AppWrapper = styled.div`
+  header {
+    background: teal;
+    &:hover {
+      background: red;
+    }
+  }
+`
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <AppWrapper>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Heading>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </Heading>
+        <Button type='cancel'>Save</Button>
+        <Button type='save'>Save</Button>
+
+        <Heading>Heading two</Heading>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
-    </div>
-  );
+    </AppWrapper>
+  )
 }
 
-export default App;
+export default App
